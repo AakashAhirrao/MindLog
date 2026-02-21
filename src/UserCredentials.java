8d5708067ff38f6cbdd740b8c298b193406f45f7
Aakash Ahirrao <ahirraoaakashni3@gmail.com>
2026-02-22 00:16:38 +0530

    User authentication service

---
+++ src/UserCredentials.java
@@ -0,0 +1,11 @@
+// We use this class JUST to catch the JSON coming from the login screen!
+public class UserCredentials {
+    private String username;
+    private String password;
+
+    public UserCredentials() {
+    }
+
+    public String getUsername() { return username; }
+    public String getPassword() { return password; }
+}
