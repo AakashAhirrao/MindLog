2a5f64a23551f2670fab7bec68b805ba112cab91
Aakash Ahirrao <ahirraoaakashni3@gmail.com>
2026-02-22 03:35:18 +0530

    Hosting on Render

--- src/DatabaseConnection.java
+++ src/DatabaseConnection.java
@@ -9,7 +9,17 @@ public class DatabaseConnection {
     private static final String PASSWORD = "Aakash@#23";
 
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+        String url = System.getenv("DB_URL");
+        String user = System.getenv("DB_USER");
+        String password = System.getenv("DB_PASSWORD");
+
+        if(url == null){
+            url = "jdbc:mysql://localhost:3306/journal_db";
+            user = "root";
+            password = "yAakash@#23";
+        }
+
+        return DriverManager.getConnection(url, user, password);
     }
 
 }
